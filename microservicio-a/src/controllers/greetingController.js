04b81d4540257d010b860228bc3c137dc3c486b8
kfsarango <kleverfsarango@gmail.com>
2021-06-24 11:25:09 -0500

    :sparkles: Add optional params to greeting route

--- microservicio-a/src/controllers/greetingController.js
+++ microservicio-a/src/controllers/greetingController.js
@@ -6,12 +6,18 @@ const {v4: uuidv4} = require('uuid');
 const queue_name = 'greeting_queue';
 
 greetingController.greeting = async (req, res) => {
-    uuid = uuidv4();
-    rabbitController.sendMessage(queue_name, uuid, 'klever');
-    console.log('this.state.')
+    // Get value of param
+    let {name} = req.query;
+    if (!name){
+        name = '';
+    }
+    // Generate uuid value
+    const uuid = uuidv4();
 
+    // add msg to queue and get response
+    const message_response = await rabbitController.sendMessage(queue_name, uuid, name);
 
-    return res.json({'msg': uuid})
+    return res.json({'message': message_response})
 }
 
 module.exports = greetingController
--- microservicio-a/src/routes/index.js
+++ microservicio-a/src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const greetingController = require('../controllers/greetingController');
 
-router.get('/greeting', greetingController.greeting);
+router.get('/greeting/:name?', greetingController.greeting);
 
 module.exports = router;
 
